ffce79546754625aba038b2493405761737598d3
spring98 <foreverspring98@gmail.com>
2024-06-03 20:47:43 +0900

    hash table 추가

--- main.py
+++ main.py
@@ -1,58 +1,28 @@
-def solution(board):
-    n = len(board)
-    dx = [0, 1, 0, -1]
-    dy = [1, 0, -1, 0]
+def solution(sequence):
+    seq_length = len(sequence)
 
-    all_paths = []
+    def makePulse(data):
+        pulse1 = []
+        pulse2 = []
 
-    def dfs(x, y, path):
-        path.append((x, y))
+        for i in range(len(data)):
+            if i % 2 == 0:
+                pulse1.append(data[i])
+                pulse2.append(-data[i])
+            else:
+                pulse1.append(-data[i])
+                pulse2.append(data[i])
 
-        if x == n - 1 and y == n - 1:
-            all_paths.append(path.copy())
-            path.pop()
-            print(path)
-            return
+        return pulse1, pulse2
 
-        for i in range(4):
-            nx = x + dx[i]
-            ny = y + dy[i]
-            if 0 <= nx < n and 0 <= ny < n and (nx, ny) not in path and board[ny][nx] == 0:
-                dfs(nx, ny, path)
+    prefix1, prefix2 = makePulse(sequence.copy())
 
-        path.pop()
+    dp = [0] * seq_length
+    dp[0] = prefix2[0]
 
-    dfs(0, 0, [])
+    for i in range(1, seq_length):
+        dp[i] = max(prefix2[i], prefix2[i] + dp[i - 1])
+        print(dp, prefix2[i], prefix2[i] + dp[i - 1])
 
-    print(all_paths)
-
-    costs = []
-    for paths in all_paths:
-        prev = ''
-        cost = 0
-        for i in range(len(paths)-1):
-            startX, startY = paths[i]
-            endX, endY = paths[i+1]
-            curr = ''
-
-            if abs(endX - startX) == 1:
-                curr = 'H'
-
-            if abs(endY - startY) == 1:
-                curr = 'V'
-
-            if curr == 'H' and prev == 'V' or curr == 'V' and prev == 'H':
-                cost += 500
-
-            cost += 100
-            prev = curr
-
-        costs.append(cost)
-
-    return min(costs)
-
-# print(f'result: {solution([[0,0,0],[0,0,0],[0,0,0]])}')
-print(f'result: {solution([[0,0,0,0,0,0,0,1],[0,0,0,0,0,0,0,0],[0,0,0,0,0,1,0,0],[0,0,0,0,1,0,0,0],[0,0,0,1,0,0,0,1],[0,0,1,0,0,0,1,0],[0,1,0,0,0,1,0,0],[1,0,0,0,0,0,0,0]])}')
-# print(f'result: {solution([[0,0,1,0],[0,0,0,0],[0,1,0,1],[1,0,0,0]])}')
-# print(f'result: {solution([[0,0,0,0,0,0],[0,1,1,1,1,0],[0,0,1,0,0,0],[1,0,0,1,0,1],[0,1,0,0,0,1],[0,0,0,0,0,0]])}')
-
+    print(dp)
+print(f'result: {solution([2, 3, -6, 1, 3, -1, 2, 4])}')
