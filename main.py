9e216541dd863a4bc9333853c473bb6b5a485944
spring98 <foreverspring98@gmail.com>
2024-06-16 19:50:17 +0900

    [Level 3] 최적의 행렬 곱셈

--- main.py
+++ main.py
@@ -1,122 +1,38 @@
-def solution(commands):
-    answer = []
-    N = 50
-    table = [['' for _ in range(N+1)] for _ in range(N+1)]
-    merge_map = []
+from itertools import permutations
+def solution(matrix_sizes):
+    table = []
 
-    def update(comm):
-        # 모든 value1 를 value2 로 변경
-        if len(comm) == 3:
-            value1, value2 = comm[1], comm[2]
-            for r in range(N + 1):
-                for c in range(N + 1):
-                    if table[r][c] == value1:
-                        table[r][c] = value2
+    for i, matrix in enumerate(matrix_sizes):
+        a, b = matrix
+        if i == 0:
+            table.append(a)
 
-        # (r, c) 의 값을 value 로 변경
-        elif len(comm) == 4:
-            r, c, value = int(comm[1]), int(comm[2]), comm[3]
-            myIndex = -1
-            for i, m in enumerate(merge_map):
-                if (r, c) in m:
-                    myIndex = i
-                    break
+        elif i == len(matrix_sizes)-1:
+            table.append(a)
+            table.append(b)
 
-            if myIndex == -1:
-                table[r][c] = value
-            else:
-                for _r, _c in merge_map[myIndex]:
-                    table[_r][_c] = value
-
-    def merge(r1, c1, r2, c2):
-        if (r1, c1) == (r2, c2):
-            return
-
-        index1 = -1
-        index2 = -1
-        for i, m in enumerate(merge_map):
-            if (r1, c1) in m:
-                index1 = i
-
-            if (r2, c2) in m:
-                index2 = i
-
-        if index1 != -1 and index2 != -1:
-            merge_map[index1] = merge_map[index1].union(merge_map[index2])
-            merge_map.pop(index2)
-
-        if index1 != -1 and index2 == -1:
-            merge_map[index1].add((r2, c2))
-
-        if index1 == -1 and index2 != -1:
-            merge_map[index2].add((r1, c1))
-
-        if index1 == -1 and index2 == -1:
-            merge_map.append({(r1, c1), (r2, c2)})
-
-        # print(merge_map, len(merge_map), index1, index2, r1, c1, r2, c2)
-        myIndex = -1
-        myValue = ''
-        for i, m in enumerate(merge_map):
-            if (r1, c1) in m:
-                myIndex = i
-                break
-
-        if myIndex != -1:
-            if table[r1][c1]:
-                for r, c in merge_map[myIndex]:
-                    table[r][c] = table[r1][c1]
-            else:
-                for r, c in merge_map[myIndex]:
-                    if table[r][c]:
-                        myValue = table[r][c]
-                        break
-
-                for r, c in merge_map[myIndex]:
-                    table[r][c] = myValue
-
-    def unmerge(param_r, param_c):
-        myIndex = -1
-        myValue = table[param_r][param_c]
-        for i, m in enumerate(merge_map):
-            if (param_r, param_c) in m:
-                myIndex = i
-                break
-
-        if myIndex != -1:
-            for r, c in merge_map[myIndex]:
-                table[r][c] = ''
-
-        table[param_r][param_c] = myValue
-
-    def table_print(r, c):
-        if table[r][c]:
-            answer.append(table[r][c])
         else:
-            answer.append('EMPTY')
+            table.append(a)
 
-    def pppp():
-        for t in table:
-            print(t)
-        print()
+    total_sum = 100000000
+    for perm in permutations(range(1, len(table)-1), len(table)-2):
+        my_table = table.copy()
+        my_table_sum = 0
+        for p in list(perm):
+            prev, cur, next = p-1, p, p+1
 
-    for command in commands:
-        comm = list(command.split(' '))
-        if comm[0] == 'UPDATE':
-            update(comm)
+            while not my_table[prev]:
+                prev -= 1
 
-        elif comm[0] == 'MERGE':
-            merge(int(comm[1]), int(comm[2]), int(comm[3]), int(comm[4]))
+            while not my_table[next]:
+                next += 1
 
-        elif comm[0] == 'UNMERGE':
-            unmerge(int(comm[1]), int(comm[2]))
+            my_table_sum += my_table[prev] * my_table[cur] * my_table[next]
+            my_table[cur] = 0
 
-        # (r, c) 의 값을 출력
-        elif comm[0] == 'PRINT':
-            table_print(int(comm[1]), int(comm[2]))
+        total_sum = min(total_sum, my_table_sum)
 
-    # pppp()
-    return answer
+    return total_sum
 
-print(f'result: {solution(["UPDATE 1 1 menu", "UPDATE 1 2 category", "UPDATE 2 1 bibimbap", "UPDATE 2 2 korean", "UPDATE 2 3 rice", "UPDATE 3 1 ramyeon", "UPDATE 3 2 korean", "UPDATE 3 3 noodle", "UPDATE 3 4 instant", "UPDATE 4 1 pasta", "UPDATE 4 2 italian", "UPDATE 4 3 noodle", "MERGE 1 2 1 3", "MERGE 1 3 1 4", "UPDATE korean hansik", "UPDATE 1 3 group", "UNMERGE 1 4", "PRINT 1 3", "PRINT 1 4"])}')
-# print(f'result: {solution(["UPDATE 1 1 a", "UPDATE 1 2 b", "UPDATE 2 1 c", "UPDATE 2 2 d", "MERGE 1 1 1 2", "MERGE 2 2 2 1", "MERGE 2 1 1 1", "PRINT 1 1", "UNMERGE 2 2", "PRINT 1 1"])}')
+# print(f'result: {solution([[5,3],[3,10],[10,6]])}')
+print(f'result: {solution([[5,3],[3,10],[10,6],[6,1]])}')
