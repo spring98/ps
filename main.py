1c67cbe543bf1118c4b98fbb4181c446f75c0254
spring98 <foreverspring98@gmail.com>
2024-06-21 12:38:11 +0900

    [Level 3] 등대 - Tree+DP

--- main.py
+++ main.py
@@ -1,61 +1,31 @@
-import heapq, math
+import sys
+sys.setrecursionlimit(10 ** 9)
 
-# def count_divisors_sqrt(n):
-#     count = 0
-#     for i in range(1, int(math.sqrt(n)) + 1):
-#         if n % i == 0:
-#             if i == n // i:
-#                 count += 1  # i와 n//i가 같은 경우 (완전 제곱수)
-#             else:
-#                 count += 2  # i와 n//i가 다른 경우
-#     return count
+def solution(n, lighthouse):
+    visited = [False] * (n + 1)
+    dp = [[0, 0] for _ in range(n + 1)]
 
-def solution(e, starts):
-    answers = []
-    myStarts = []
+    graph = [[] for _ in range(n + 1)]
+    for u, v in lighthouse:
+        graph[u].append(v)
+        graph[v].append(u)
 
-    for i, start in enumerate(starts):
-        myStarts.append((start, i))
+    def dfs(x):
+        visited[x] = True
+        dp[x][0] = 0
+        dp[x][1] = 1
 
-    myStarts.sort()
+        for i in graph[x]:
+            if not visited[i]:
+                dfs(i)
+                dp[x][0] += dp[i][1]
+                dp[x][1] += min(dp[i][0], dp[i][1])
 
-    table = [0] * (e + 1)
-    for i in range(2, e + 1):
-        for j in range(1, min(e // i + 1, i)):
-            table[i * j] += 2
-    for i in range(1, int(e ** (1 / 2)) + 1):
-        table[i ** 2] += 1
+    dfs(1)
+    answer = min(dp[1][0], dp[1][1])
 
-    # table = [-1]
-    # for n in range(1, e+1):
-    #     table.append(count_divisors_sqrt(n))
+    return answer
 
-    # print(table)
-    myHeap = []
-    for i, t in enumerate(table):
-        heapq.heappush(myHeap, (-t, i))
-
-    for start, priority in myStarts:
-        while myHeap:
-            value, index = heapq.heappop(myHeap)
-
-            if start <= index:
-                answers.append((priority, index))
-                heapq.heappush(myHeap, (value, index))
-                break
-
-    answers.sort()
-    # print(answers)
-    re = []
-    for p, value in answers:
-        re.append(value)
-
-    return re
-
-# print(f'result: {solution(8,[1,3,7])}')
-print(f'result: {solution(8,[7,3,1])}')
-# print(f'result: {solution(15,[7,8])}')
-# print(f'result: {solution(11,[6,8])}')
-# print(f'result: {solution(1000,[])}')
+print(f'result: {solution(8,[[1, 2], [1, 3], [1, 4], [1, 5], [5, 6], [5, 7], [5, 8]])}')
 
 
